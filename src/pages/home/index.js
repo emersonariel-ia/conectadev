578bf753618cf6fe8e2139781ad0fd20e5c58360
Emerson Ariel <emersonariel123@gmail.com>
2020-12-15 13:11:24 -0300

    Initial Code

---
+++ src/pages/home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+import Header from './components/header';
+
+function Home(){
+    return(
+        <div>
+            <Header />
+            <main className="main">
+                <div className="navbar">
+                    navbar
+                </div>
+                <div className="feed">
+                    feed
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Home;
